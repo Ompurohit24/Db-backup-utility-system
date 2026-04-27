5a4e9f78c2764390e24b2af3a137a83f4b4083cd
Om Purohit <120297002+Ompurohit24@users.noreply.github.com>
2026-04-27 09:40:33 +0530

    Refactor FastAPI app initialization and CORS setup

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.logging_setup import setup_logging
 from app.routes.auth import router as auth_router
@@ -10,10 +10,10 @@ from app.routes.backup import router as backup_router
 from app.routes.schedule import router as schedule_router
 from app.routes.admin import router as admin_router
 from app.routes.notifications import router as notifications_router
+from app.routes.logs import router as logs_router
+
 from app.services.schedule_service import load_active_schedules
 from app.utils.scheduler import scheduler_shutdown, scheduler_startup
-from fastapi.middleware.cors import CORSMiddleware
-from app.routes.logs import router as logs_router
 
 
 @asynccontextmanager
@@ -25,7 +25,32 @@ async def lifespan(app: FastAPI):
     await scheduler_shutdown()
 
 
-app = FastAPI(title="Database Backup Utility", version="1.0", lifespan=lifespan)
+app = FastAPI(
+    title="Database Backup Utility",
+    version="1.0",
+    lifespan=lifespan
+)
+
+# ✅ CORS FIRST
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:5500",
+    "http://127.0.0.1:5501",
+    "http://localhost:5500",
+    "http://localhost:5501",
+    "https://vaultdb.live",
+    "https://www.vaultdb.live"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# ✅ Then routes
 app.include_router(logs_router)
 app.include_router(auth_router)
 app.include_router(user_router)
@@ -39,24 +64,3 @@ app.include_router(notifications_router)
 @app.get("/")
 def home():
     return {"message": "Database Backup Utility API Running"}
-
-# ✅ Allowed frontend URLs
-origins = [
-    "http://localhost:3000",
-    "http://127.0.0.1:5500",
-    "http://127.0.0.1:5501",
-    "http://localhost:5501",
-    "http://localhost:5500",
-
-    "https://vaultdb.live",
-    "https://www.vaultdb.live"
-]
-
-# ✅ Add CORS middleware
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
